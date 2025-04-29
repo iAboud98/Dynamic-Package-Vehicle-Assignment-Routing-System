2499b910e7ea2ebb5f540e36d25975bdf772fb00
iAboud98 <aboudfialah98@gmail.com>
2025-04-30 00:22:33 +0300

    create fitness function

--- algorithms/genetic_algorithm.py
+++ algorithms/genetic_algorithm.py
@@ -1,3 +1,6 @@
+#Aboud Fialah           ID: 1220216         Section: 2
+#Aws Hammad             ID: 1221697         Section: 3
+
 import random
 import math
 
@@ -27,8 +30,13 @@ def genetic_algorithm(manager, params):
     for _ in range(params["population_size"]):
         individual = generate_individual(packages,vehicles)
         population.append(individual)
-
-
+    
+    for p in population:
+        vehs, dis = evaluate_fitness_func(p, packages, vehicles)
+        print(vehs)
+        print(dis)
+        print()
+    
 
 
 
@@ -61,9 +69,7 @@ def generate_individual(packages, vehicles):        #-> function to generate ind
 def calculate_route_distance(packages):        #-> function to calculate route distance for vehicle
 
     if not packages:
-        return 0
-
-    packages.sort(key=lambda p: p.priority)     #-> sort based on priority
+        return 0.0
 
     current_pos = [0,0]             #-> Shop location 
     total_distance = 0.0            #-> initialize distance
@@ -73,9 +79,64 @@ def calculate_route_distance(packages):        #-> function to calculate route d
         current_pos = [package.x, package.y]
         total_distance += distance
     
-    return total_distance           #return total distance, but with out return distance !!
+    return total_distance           #-> return total distance, but with out return distance !!
 
-def euclidean_distance(x1, y1, x2, y2):     
-    return math.sqrt((x1 - y1)**2 + (x2 - y2)**2)
+def euclidean_distance(x1, y1, x2, y2):             #-> function to calculate the distance between two points
+    return math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+
+
+def evaluate_fitness_func(indiviual, packages, vehicles):
+    
+
+    total_distance = 0.0
+    priority_violation = 0.0
+
+
+    vehicle_assignment = {}
+
+    for v in vehicles:
+        vehicle_assignment[v.id] = []
 
     
+    for pckg_idx, veh_id in enumerate(indiviual):
+        vehicle_assignment[veh_id].append(packages[pckg_idx].id)
+
+
+    for veh_id, pckgs_id in vehicle_assignment.items():
+
+        random.shuffle(pckgs_id)
+
+        pckgs = []
+
+        for id in pckgs_id:
+            pckgs.append(packages[id-1])
+        
+        total_distance += calculate_route_distance(pckgs)
+
+        priority_violation += calculate_priority_violation(pckgs, total_distance)
+
+        fitness_function = total_distance + priority_violation
+
+
+    return vehicle_assignment, fitness_function
+
+    
+def calculate_priority_violation (packages, route_distance):
+    
+    if len(packages) == 1:
+        return 0.0
+    
+    violation_value = 0.0
+
+    violation_unit = route_distance * 0.1
+
+
+    sorted_packages = sorted(packages, key=lambda p: p.priority)
+
+    
+    for idx, pckg in enumerate(sorted_packages):
+        if packages[idx].priority > pckg.priority:
+            priority_diff = packages[idx].priority - pckg.priority
+            violation_value += priority_diff * violation_unit
+    
+    return violation_value
